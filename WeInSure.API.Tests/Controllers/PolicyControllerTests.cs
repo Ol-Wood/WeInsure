7c4ffc46e047146873e9263c18f484bcfb1bd3fb
Oliver Wood <oa.wood@outlook.com>
2026-02-21 14:16:50 +0000

    Added full controller endpoint coverage for sell policy

--- WeInSure.API.Tests/Controllers/PolicyControllerTests.cs
+++ WeInSure.API.Tests/Controllers/PolicyControllerTests.cs
@@ -13,6 +13,24 @@ public class PolicyControllerTests
 {
     private readonly ISellPolicyUseCase _sellPolicyUseCase = Substitute.For<ISellPolicyUseCase>();
     private readonly PolicyController _policyController;
+    private readonly SellPolicyCommand _sellPolicyCommand =  new()
+    {
+        Amount = 100,
+        PolicyType = PolicyType.Household,
+        StartDate = DateOnly.FromDateTime(DateTime.UtcNow),
+        Payment = new PaymentDto
+        {
+            PaymentReference = "PayRef123",
+            Amount = 100,
+            PaymentType = PaymentType.Card
+        },
+        PolicyHolders = [new PolicyHolderDto
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            DateOfBirth = DateOnly.FromDateTime(new DateTime(1990, 1, 1)),
+        }]
+    };
 
     public PolicyControllerTests()
     {
@@ -25,33 +43,44 @@ public class PolicyControllerTests
     {
         var policyId = Guid.NewGuid();
         var policyReference = Guid.NewGuid().ToString();
-        var command = new SellPolicyCommand
-        {
-            Amount = 100,
-            PolicyType = PolicyType.Household,
-            StartDate = DateOnly.FromDateTime(DateTime.UtcNow),
-            Payment = new PaymentDto
-            {
-                PaymentReference = "PayRef123",
-                Amount = 100,
-                PaymentType = PaymentType.Card
-            },
-            PolicyHolders = [new PolicyHolderDto
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                DateOfBirth = DateOnly.FromDateTime(new DateTime(1990, 1, 1)),
-            }]
-        };
         
         var sellPolicyResultDto = new SoldPolicy(policyId, policyReference);
-        _sellPolicyUseCase.Execute(command).Returns(Result<SoldPolicy>.Success(sellPolicyResultDto));
+        _sellPolicyUseCase.Execute(_sellPolicyCommand).Returns(Result.Success(sellPolicyResultDto));
 
-        var result = await _policyController.SellPolicy(command);
+        var result = await _policyController.SellPolicy(_sellPolicyCommand);
         
         var expectedResponse = new SoldPolicy(policyId, policyReference);
         var okResult = Assert.IsType<OkObjectResult>(result);
         Assert.Equal(expectedResponse, okResult.Value);
+    }
+    
+    
+    [Fact]
+    public async Task SellPolicy_Returns400WithErrorMessage_WhenUseCaseReturnsValidationError()
+    {
+        const string errorMessage = "Policy holder not old enough";
+        
+        _sellPolicyUseCase.Execute(_sellPolicyCommand)
+            .Returns(Result<SoldPolicy>.Failure(Error.Validation(errorMessage)));
+
+        var result = await _policyController.SellPolicy(_sellPolicyCommand);
+        
+        var badResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(errorMessage, badResult.Value);
+    }
+    
+    [Fact]
+    public async Task SellPolicy_Returns400WithErrorMessage_WhenUseCaseReturnsDomainError()
+    {
+        const string errorMessage = "Domain Error";
+        
+        _sellPolicyUseCase.Execute(_sellPolicyCommand)
+            .Returns(Result<SoldPolicy>.Failure(Error.Domain(errorMessage)));
+
+        var result = await _policyController.SellPolicy(_sellPolicyCommand);
+        
+        var badResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(errorMessage, badResult.Value);
     }
 }
 
--- WeInsure.API/Controllers/PolicyController.cs
+++ WeInsure.API/Controllers/PolicyController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WeInsure.Application.Policy.Commands;
 using WeInsure.Application.Policy.UseCases.Interfaces;
+using WeInsure.Domain.Shared;
 
 namespace WeInsure.API.Controllers;
 
@@ -11,10 +12,22 @@ public class PolicyController(ISellPolicyUseCase sellPolicyUseCase) : Controller
     
     [HttpPost("sell")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult> SellPolicy([FromBody] SellPolicyCommand command)
     {
         var result = await sellPolicyUseCase.Execute(command);
-        
-        return Ok(result.Data);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Data);
+        }
+
+        return result.Error?.Type switch
+        {
+            ErrorType.Validation => BadRequest(result.Error.Message),
+            ErrorType.Domain => BadRequest(result.Error.Message),
+            _ => StatusCode(500)
+        };
     }
 }
