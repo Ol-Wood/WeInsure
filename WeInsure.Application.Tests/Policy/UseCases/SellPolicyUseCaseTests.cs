2e27fd459c890bf8cbb1acef4134d8cb83e1e964
Oliver Wood <oa.wood@outlook.com>
2026-02-21 14:58:37 +0000

    Added tests for checking policy start date

--- WeInsure.Application.Tests/Policy/UseCases/SellPolicyUseCaseTests.cs
+++ WeInsure.Application.Tests/Policy/UseCases/SellPolicyUseCaseTests.cs
@@ -26,4 +26,29 @@ public class SellPolicyUseCaseTests
         Assert.NotNull(result.Error);
         Assert.Equal(ErrorType.Validation, result.Error.Type);
     }
+
+
+    [Theory]
+    [InlineData(61)]
+    [InlineData(62)]
+    [InlineData(99)]
+    [InlineData(10222)]
+    public async Task SellPolicy_ShouldReturnResultDomainError_IfPolicyStartDateIsMoreThan60DaysInTheFuture(int daysInFuture)
+    {
+        var command = new WeInsureFixture()
+            .Build<SellPolicyCommand>()
+            .With(x => x.StartDate, DateOnly.FromDateTime(DateTime.UtcNow.AddDays(daysInFuture)))
+            .Create();
+        
+        var validator = Substitute.For<IValidator<SellPolicyCommand>>();
+        validator.ValidateAsync(command).Returns(new ValidationResult());
+        var useCase = new SellPolicyUseCase(validator);
+        
+        var result = await useCase.Execute(command);
+        
+        Assert.Null(result.Data);
+        Assert.NotNull(result.Error);
+        Assert.Equal(ErrorType.Domain, result.Error.Type);
+        Assert.Equal("Policy start date can't be more than 60 days in the future", result.Error.Message);
+    }
 }
--- WeInsure.Application/Policy/UseCases/SellPolicyUseCase.cs
+++ WeInsure.Application/Policy/UseCases/SellPolicyUseCase.cs
@@ -11,12 +11,21 @@ public class SellPolicyUseCase(IValidator<SellPolicyCommand> validator) : ISellP
     public async Task<Result<SoldPolicy>> Execute(SellPolicyCommand command)
     {
         var validationResult = await validator.ValidateAsync(command);
-
         if (!validationResult.IsValid)
         {
             return Result<SoldPolicy>.Failure(Error.Validation("error"));
         }
+
+
+        if (command.StartDate > DateOnly.FromDateTime(DateTime.UtcNow.AddDays(60)))
+        {
+            return Result<SoldPolicy>.Failure(Error.Domain("Policy start date can't be more than 60 days in the future")); 
+        }
+        
+        
+        
         
         throw new NotImplementedException();
     }
+    
 }
