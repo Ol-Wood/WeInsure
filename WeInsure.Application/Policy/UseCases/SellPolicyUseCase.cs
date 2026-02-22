e13840356c730a30363caa55c6b73b210012d672
Oliver Wood <oa.wood@outlook.com>
2026-02-22 11:21:06 +0000

    Refactor sell policy use case

--- WeInsure.Application/Policy/UseCases/SellPolicyUseCase.cs
+++ WeInsure.Application/Policy/UseCases/SellPolicyUseCase.cs
@@ -5,6 +5,7 @@ using WeInsure.Application.Policy.UseCases.Interfaces;
 using WeInsure.Domain.Entities;
 using WeInsure.Domain.Shared;
 using WeInsure.Domain.ValueObjects;
+using PolicyEntity = WeInsure.Domain.Entities.Policy;
 
 namespace WeInsure.Application.Policy.UseCases;
 
@@ -30,27 +31,38 @@ public class SellPolicyUseCase(IValidator<SellPolicyCommand> validator) : ISellP
             return Result<SoldPolicy>.Failure(paidPrice.Error);
         }
 
-        var policyHolders = new List<PolicyHolder>();
-        foreach (var holderDto in command.PolicyHolders)
+        var policyHolders = CreatePolicyHolders(command);
+        if (!policyHolders.IsSuccess)
         {
-           var holder = PolicyHolder.Create(holderDto.FirstName, holderDto.LastName, holderDto.DateOfBirth);
-
-           if (!holder.IsSuccess)
-           {
-               return Result<SoldPolicy>.Failure(holder.Error);
-           }
-           
-           policyHolders.Add(holder.Data);
+            return Result<SoldPolicy>.Failure(policyHolders.Error);
         }
         
-
-        var policy = Domain.Entities.Policy.Create("Ref", command.StartDate, policyHolders.ToArray(), policyPrice.Data);
+        var policy = PolicyEntity.Create("Ref", command.StartDate, policyHolders.Data, policyPrice.Data);
         if (!policy.IsSuccess)
         {
             return Result<SoldPolicy>.Failure(policy.Error);
         }
         
         throw new NotImplementedException();
+    }
+
+
+    private Result<PolicyHolder[]> CreatePolicyHolders(SellPolicyCommand command)
+    {
+        var policyHolders = new List<PolicyHolder>();
+        foreach (var holderDto in command.PolicyHolders)
+        {
+            var holder = PolicyHolder.Create(holderDto.FirstName, holderDto.LastName, holderDto.DateOfBirth);
+
+            if (!holder.IsSuccess)
+            {
+                return Result<PolicyHolder[]>.Failure(holder.Error);
+            }
+           
+            policyHolders.Add(holder.Data);
+        }
+        
+        return Result<PolicyHolder[]>.Success(policyHolders.ToArray());
     }
     
 }
