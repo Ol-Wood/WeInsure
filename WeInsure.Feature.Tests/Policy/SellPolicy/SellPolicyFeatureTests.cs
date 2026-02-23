534fb454bb2bb0ad6ea18425f673489755578466
Oliver Wood <oa.wood@outlook.com>
2026-02-23 17:03:03 +0000

    Added unhappy path for sell policy feature test

--- WeInsure.Feature.Tests/Policy/SellPolicy/SellPolicyFeatureTests.cs
+++ WeInsure.Feature.Tests/Policy/SellPolicy/SellPolicyFeatureTests.cs
@@ -15,6 +15,34 @@ public class SellPolicyFeatureTests
 {
     private readonly PolicyController _controller;
     private readonly IPolicyRepository _policyRepository = Substitute.For<IPolicyRepository>();
+    private readonly SellPolicyCommand _validCommand = new(){
+        Amount = 100,
+        PolicyType = PolicyType.Household,
+        StartDate = DateOnly.FromDateTime(DateTime.UtcNow),
+        Payment = new PaymentDto
+        {
+            PaymentReference = "PayRef123",
+            Amount = 100,
+            PaymentType = PaymentType.Card
+        },
+        PolicyHolders =
+        [
+            new PolicyHolderDto
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                DateOfBirth = DateOnly.FromDateTime(new DateTime(1990, 1, 1)),
+            }
+        ],
+        PolicyAddress = new AddressDto
+        {
+            AddressLine1 = "AddressLine1",
+            AddressLine2 = "AddressLine2",
+            AddressLine3 = "AddressLine3",
+            PostCode = "PostCode",
+        },
+        AutoRenew = false,
+    };
 
     public SellPolicyFeatureTests()
     {
@@ -27,38 +55,19 @@ public class SellPolicyFeatureTests
     [Fact]
     public async Task SellPolicyShould_SuccessfullyCreatePolicy()
     {
-        var command = new SellPolicyCommand
-        {
-            Amount = 100,
-            PolicyType = PolicyType.Household,
-            StartDate = DateOnly.FromDateTime(DateTime.UtcNow),
-            Payment = new PaymentDto
-            {
-                PaymentReference = "PayRef123",
-                Amount = 100,
-                PaymentType = PaymentType.Card
-            },
-            PolicyHolders =
-            [
-                new PolicyHolderDto
-                {
-                    FirstName = "John",
-                    LastName = "Doe",
-                    DateOfBirth = DateOnly.FromDateTime(new DateTime(1990, 1, 1)),
-                }
-            ],
-            PolicyAddress = new AddressDto
-            {
-                AddressLine1 = "AddressLine1",
-                AddressLine2 = "AddressLine2",
-                AddressLine3 = "AddressLine3",
-                PostCode = "PostCode",
-            },
-            AutoRenew = false,
-        };
-
-        var result = await _controller.SellPolicy(command);
+        var result = await _controller.SellPolicy(_validCommand);
 
         Assert.IsType<OkObjectResult>(result);
+    }
+
+
+    [Fact]
+    public async Task SellPolicyShould_ReturnBadRequest_WhenValidationFails()
+    {
+        var invalidCommand = _validCommand with { Amount = -100 };
+        
+        var result = await _controller.SellPolicy(invalidCommand);
+        
+        Assert.IsType<BadRequestObjectResult>(result);
     }
 }
