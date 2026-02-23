5524db827a77d67bbee0f5e21d78c80e7e40c320
Oliver Wood <oa.wood@outlook.com>
2026-02-23 21:32:20 +0000

    Register services

--- WeInsure.API/Program.cs
+++ WeInsure.API/Program.cs
@@ -1,6 +1,15 @@
+using WeInsure.Application.Policy.UseCases;
+using WeInsure.Application.Policy.UseCases.Interfaces;
+using WeInsure.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IGetPolicyUseCase, GetPolicyUseCase>();
+builder.Services.AddScoped<ISellPolicyUseCase, SellPolicyUseCase>();
+builder.Services.AddScoped<IPolicyReferenceGenerator, PolicyReferenceGenerator>();
+
+builder.Services.AddSingleton<IIdGenerator, IdGenerator>();
 
 var app = builder.Build();
 
