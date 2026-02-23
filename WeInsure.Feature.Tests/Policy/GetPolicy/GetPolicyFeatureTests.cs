3ae228aacd306fbf9a01e06acee82761e932c134
Oliver Wood <oa.wood@outlook.com>
2026-02-23 20:19:58 +0000

    Feature test for get policy happy path now passing

--- WeInsure.Feature.Tests/Policy/GetPolicy/GetPolicyFeatureTests.cs
+++ WeInsure.Feature.Tests/Policy/GetPolicy/GetPolicyFeatureTests.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using WeInsure.API.Controllers;
+using WeInsure.Application.Policy.Dtos;
+using WeInsure.Application.Policy.Mappers;
 using WeInsure.Application.Policy.UseCases;
 using WeInsure.Application.Policy.UseCases.Interfaces;
+using WeInsure.Domain.Entities;
+using WeInsure.Domain.Enums;
 using WeInsure.Domain.Repositories;
+using WeInsure.Domain.Shared;
 using WeInsure.Domain.ValueObjects;
 
 namespace WeInsure.Feature.Tests.Policy.GetPolicy;
@@ -15,16 +20,37 @@ public class GetPolicyFeatureTests
 
     public GetPolicyFeatureTests()
     {
-        _controller = new PolicyController(Substitute.For<ISellPolicyUseCase>(), new GetPolicyUseCase(_policyRepository));
+        _controller =
+            new PolicyController(Substitute.For<ISellPolicyUseCase>(), new GetPolicyUseCase(_policyRepository));
     }
-    
+
     [Fact]
     public async Task GetPolicyFeature_Returns200_WithPolicy_WhenSuccessful()
     {
+        var policy = CreatePolicy();
+        var policyReference = PolicyReference.Create().Value;
+        _policyRepository.GetByReference(policyReference).Returns(policy);
+
+        var result = await _controller.GetPolicy(policyReference);
+
+        var objectResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equivalent(policy.ToDto(), objectResult.Value);
+    }
+
+    private static Domain.Entities.Policy CreatePolicy()
+    {
+        var policyId = Guid.NewGuid();
         var policyReference = PolicyReference.Create();
-        
-        var result = await _controller.GetPolicy(policyReference.Value);
-        
-        Assert.IsType<OkObjectResult>(result);
+        var moneyAmount = Money.Create(2000).OrThrow();
+        var policyHolder =
+            PolicyHolder.Create(Guid.NewGuid(), policyId, "John", "Doe", new DateOnly(1990, 01, 01)).OrThrow();
+        var address = Address.Create("Line1", "Line2", "Line3", "M34 7ER").OrThrow();
+        var insuredProperty = InsuredProperty.Create(Guid.NewGuid(), policyId, address);
+        var payment = Payment.Create(Guid.NewGuid(), policyId, PaymentType.Card, moneyAmount, "REF").OrThrow();
+
+        var policy = Domain.Entities.Policy.Create(policyId, policyReference, DateOnly.FromDateTime(DateTime.UtcNow),
+            PolicyType.Household, [policyHolder], moneyAmount, insuredProperty, payment);
+
+        return policy.OrThrow();
     }
 }
